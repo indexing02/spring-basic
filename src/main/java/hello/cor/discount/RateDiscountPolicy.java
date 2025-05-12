b49f613d06292920b4aa74a5be68f2dbf77b508d
indexing02 <indexing02@gmail.com>
2025-05-13 01:52:14 +0900

    0513 컴포넌트 스캔

--- src/main/java/hello/cor/discount/RateDiscountPolicy.java
+++ src/main/java/hello/cor/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.cor.discount;
 
 import hello.cor.member.Grade;
 import hello.cor.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy  implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/hello/cor/order/OrderServiceImpl.java
+++ src/main/java/hello/cor/order/OrderServiceImpl.java
@@ -4,12 +4,16 @@ import hello.cor.discount.DiscountPolicy;
 import hello.cor.member.Member;
 import hello.cor.member.MemberRepository;
 import hello.cor.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
